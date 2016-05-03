ab302d309ad3169c97ff0f0f4dd99be51a9605d8
Jordan Taylor <jordandtaylor@gmail.com>
2016-05-03 10:02:48 -0600

    command pattern started

---
+++ src/edu/neumont/jotaylor/csc360/commands/ICommand.java
@@ -0,0 +1,19 @@
+package edu.neumont.jotaylor.csc360.commands;
+
+/**
+ * Created by jorda on 5/1/2016.
+ */
+public interface ICommand {
+//    void undo();
+//    void redo();
+}
+
+abstract class Command implements ICommand{
+
+}
+class deleteCommand extends Command{
+
+
+}
+
+
